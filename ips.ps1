a04826878f5986030e975313f4f03ed12d6dcd6e
Mark3z <markklimchenko@yandex.ru>
2021-02-25 19:06:11 +0300

    initializing repository

---
+++ ips.ps1
@@ -0,0 +1,20 @@
+﻿Function Compare-Subnets {
+param (
+[parameter(Mandatory=$true)]
+[Net.IPAddress]
+$ip1,
+ 
+[parameter(Mandatory=$true)]
+[Net.IPAddress]
+$ip2,
+ 
+[parameter()]
+[alias("SubnetMask")]
+[Net.IPAddress]
+$mask ="255.255.255.0"
+)
+ 
+if (($ip1.address -band $mask.address) -eq ($ip2.address -band $mask.address)) {$true}
+else {$false}
+ 
+}
